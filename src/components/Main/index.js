65271149de45669a1fb7dd8bc399f38749d9da63
LouisWright <louis--wright@hotmail.co.uk>
2022-11-10 16:51:35 +0000

    Completed everything up to list items

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
-
-import './index.css';
-
-import samplePosts from '../../libs/data';
-
-import Post from '../Post';
+import React, { useState } from "react";
+import "./index.css";
+import samplePosts from "../../libs/data";
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
+  let id = 0;
 
-  return <main id="main"></main>;
+  return (
+    <main id="main">
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post.postId}
+            title={post.title}
+            data={post.date}
+            author={post.author}
+            highlights={post.highlights.map((post) => {
+              return <ul text={post.highlights} key={id++}></ul>;
+            })}
+            text={post.text}
+            image={post.image}
+          />
+        );
+      })}
+    </main>
+  );
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-function Post() {
-  return <article></article>;
+function Post(props) {
+  return (
+    <article
+      key={props.postId}
+      title={props.title}
+      data={props.date}
+      author={props.author}
+      text={props.text}
+      highlights={props.highlights}
+      image={props.image}
+    >
+      <h1>{props.title}</h1>
+      <img
+        src={props.image.link}
+        alt={props.image.alt}
+        height={300}
+        width={400}
+      />
+      <p>{props.author}</p>
+      <li>{props.highlights}</li>
+      <p>{props.text}</p>
+      <div>{props.date}</div>
+    </article>
+  );
 }
 
 export default Post;
